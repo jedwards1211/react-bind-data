783119b315de575c5e722e326c29329864dbe2a7
Andy Edwards <jedwards@fastmail.com>
2016-06-22 16:13:53 -0500

    just add onChange handlers any time onFieldChange is given

--- src/BindData.js
+++ src/BindData.js
@@ -36,12 +36,10 @@ export default function bindData(options?: {
         if (metavalue != null) result[metaprop] = metavalue
       }
       if (onFieldChange) {
-        if ('data' in options) {
-          const event = prop === 'value' ? 'onChange' : `on${upperFirst(prop)}Change`
-          result[event] = e => {
-            if (e && e.target instanceof Object) onFieldChange(path, e.target.value)
-            else onFieldChange(path, e)
-          }
+        const event = prop === 'value' ? 'onChange' : `on${upperFirst(prop)}Change`
+        result[event] = e => {
+          if (e && e.target instanceof Object) onFieldChange(path, e.target.value)
+          else onFieldChange(path, e)
         }
       }
     }
